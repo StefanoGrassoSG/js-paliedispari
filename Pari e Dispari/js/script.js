a9bf8ab4df7dcbc3a0cec43274af31cee93cad09
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-20 16:23:44 +0200

    ADD function iseven

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -6,18 +6,36 @@ function getRndNumber (min, max) {
   }
 
 
-formButton.addEventListener('click' , function(e){
+
+submitForm.addEventListener('submit' , function(e){
+    e.preventDefault();
     const numberFromInput = parseInt(document.getElementById('numberFromInput').value);
+ 
 
     let computerNum = getRndNumber(1, 6);
-    console.log('numero del computer', computerNum, typeof computerNum)
     let sum = numberFromInput + computerNum;
     console.log('somma dei numeri', sum)
 
+    function isEven (sum) {
     
+        if (sum % 2 == 0) {
+            return true;
+        }
+    
+        return false;
+    }
+    
+    if (isEven(sum) == true) {
+        const result = document.querySelector('h1');
+        result.innerHTML = 'HAI VINTO!'
+    }
 
-
+    else {
+        const result = document.querySelector('h1');
+        result.innerHTML = 'HAI perso!'
+    }
     
 })
+
 
 
