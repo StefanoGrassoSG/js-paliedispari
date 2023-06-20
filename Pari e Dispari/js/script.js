41b1460bb66548492fbcf8b17ffa054deed32e3a
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-20 15:52:58 +0200

    ADD function num 1 to 5 in js

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,6 +1,10 @@
 const formButton = document.getElementById('form-button')
 
 
+function getRndNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
 
 formButton.addEventListener('click' , function(e){
     const numberFromInput = document.getElementById('numberFromInput').value;
@@ -9,4 +13,9 @@ formButton.addEventListener('click' , function(e){
     if(numberFromInput > 5 || numberFromInput < 1) {
         alert ('insert 1 to 5')
     }
+
+    console.log(getRndNumber(1, 5))
+    
 })
+
+
