8be5595fb445936002b671320dce807730a584c5
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-20 17:45:42 +0200

    ADD check first and last word

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -3,6 +3,9 @@ function checkPali(gth) {
     const len = gth.length;
 
     for (let i = 0; i < len / 2; i++) {
-        
+
+        if (gth[i] !== gth[len - 1 - i]) {
+            alert ('is not palindrome!')
+        }
     }
 }
