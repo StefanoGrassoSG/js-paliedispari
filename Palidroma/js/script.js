fa82914d318ae08fd2edf854826f0dd80c217eb5
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-20 18:18:41 +0200

    EDIT from prompt to form

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,21 +1,24 @@
+const form = document.getElementById('input-form');
 
-function checkPali(gth) {
-    const len = gth.length;
 
-    for (let i = 0; i < len / 2; i++) {
 
-        if (gth[i] !== gth[len - 1 - i]) {
-            alert ('is not palindrome!');
-            return;
+form.addEventListener('submit', function(evt){
+    evt.preventDefault;
+
+    function checkPali(gth) {
+        const len = gth.length;
+    
+        for (let i = 0; i < len / 2; i++) {
+    
+            if (gth[i] !== gth[len - 1 - i]) {
+                alert ('is not palindrome!');
+                return;
+            }
         }
+    
+        alert('it is a palindrome!')
     }
-
-    alert('it is a palindrome!')
-}
-
-const input = prompt('insert a word to check if is palindrome');
-
-
-
-const result = checkPali(input);
-
+    
+    const result = document.getElementById('word').value;
+    const value = checkPali(result);
+})
